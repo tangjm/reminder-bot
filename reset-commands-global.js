b9492e2173e53fc2d64e65373765880681a876e0
tangjm <tangjm.dev@outlook.com>
2022-11-13 16:20:22 +0000

    Add scripts for deploying slash commands globally and to specific guilds

--- reset-commands-global.js
+++ reset-commands-global.js
@@ -1,17 +1,7 @@
 require('dotenv').config();
 const { REST, Routes } = require('discord.js');
-const fs = require('node:fs');
 
 const commands = [];
-
-// Grab all the command files from the commands directory you created earlier
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
-}
 
 // Construct and prepare an instance of the REST module
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
@@ -23,7 +13,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
-			Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.TEST_GUILD_ID),
+			Routes.applicationCommands(process.env.CLIENT_ID),
 			{ body: commands },
 		);
 
