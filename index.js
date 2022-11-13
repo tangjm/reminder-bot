2bf23fb8049315c1aba37acaa439284dea691314
tangjm <tangjm.dev@outlook.com>
2022-11-13 17:10:12 +0000

    Add entry point to discord bot

--- index.js
+++ index.js
@@ -1,56 +1,77 @@
 require('dotenv').config();
+const pg = require('pg');
 const express = require('express');
 const cronJob = require('node-cron');
 const fs = require('node:fs');
 const path = require('node:path');
 
+const { install_generic_operations_on_reminders } = require('./genericOperations.js');
+
+install_generic_operations_on_reminders();
 
 // Log in Reminder Discord Bot
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.once('ready', c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+// Setup slash commands
+client.commands = new Collection();
 
-client.login(process.env.DISCORD_TOKEN);
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs
+	.readdirSync(commandsPath)
+	.filter((file) => file.endsWith('.js'));
 
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	}
+	else {
+		console.log(
+			`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
+		);
+	}
+}
 
-const currentTasks = cronJob.getTasks();
+// Setup with event handlers
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-// Import reminders and set them up if they are not already setup.
-const remindersPath = path.join(__dirname, 'reminders');
-const reminderFiles = fs
-	.readdirSync(remindersPath)
-	.filter(file => file.endsWith('Reminder.js'));
-
-for (const reminderFile of reminderFiles) {
-	const reminderFilePath = path.join(remindersPath, reminderFile);
-	const reminder = require(reminderFilePath);
-
-	if (!currentTasks.has(reminder.name)) {
-		reminder(process.env.CHANNEL_ID);
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	}
+	else {
+		client.on(event.name, (...args) => event.execute(...args));
 	}
 }
 
 
-// Express server
-const app = express();
-const PORT = process.env.PORT || 3000;
+client.login(process.env.DISCORD_TOKEN);
 
-app.get('/', (req, res) => {
-	const activeTasks = cronJob.getTasks();
 
-	const responseData = {
-		'num_of_active_jobs': activeTasks.size,
-		'active_jobs': [],
-	};
+// Connect to PostgresSQL database 
 
-	activeTasks.forEach((_, name) => responseData.active_jobs.push({ name }));
-	res.status(200).send(responseData);
-});
+// const connectionString = `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}/${process.env.PG_DATABASE}`;
 
-app.listen(PORT, () => {
-	console.log('Express server listening on port', PORT);
-});
+// const pgClient = new pg.Client(connectionString);
+
+// pgClient.connect(function(err) {
+// 	if (err) {
+// 		return console.err('Could not connect to postgres', err);
+// 	}
+// 	console.log('Ready! Copnnected to postgres');
+// 	pgClient.query('SELECT NOW() AS time', function(err, result) {
+// 		if (err) {
+// 			return console.err('Error running query', err);
+// 		}
+// 		console.log(result);
+// 		console.log(result.rows[0].time);
+// 		pgClient.end();
+// 	});
+// });
